056698f20712dfac507248a57816cc747d46c704
sri-rishi <sririshi2612@gmail.com>
2024-02-04 18:45:49 +0530

    feat: add hero section component to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { useEffect } from 'react';
 import './App.css';
-import { Navbar } from './components';
+import { HeroSection, Navbar } from './components';
 
 function App() {
 
   useEffect(() => {
     document.title = "aBit Assignment"
   }, []);
-  
+
   return (
     <div className="App">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }

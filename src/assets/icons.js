d89c7939c8e3fc63627c75236513b315d59bd287
sri-rishi <sririshi2612@gmail.com>
2024-02-04 23:51:15 +0530

    style: import images and icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,9 +1,11 @@
 import { IoWallet } from "react-icons/io5";
 import { BsFillBellFill } from "react-icons/bs";
 import { BsChevronDown } from "react-icons/bs";
+import { HiPlus } from "react-icons/hi";
 
 export {
     IoWallet,
     BsFillBellFill,
-    BsChevronDown
+    BsChevronDown,
+    HiPlus
 }
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import {IoWallet,BsFillBellFill,BsChevronDown} from "./icons";
+import {IoWallet,BsFillBellFill,BsChevronDown, HiPlus} from "./icons";
 import PROFILE_PIC from "./images/profile-pic.png";
 import COVER_IMAGE from "./images/cover-image.png";
 import COVER_BAND from "./images/cover-band.png";
@@ -7,6 +7,7 @@ export {
     IoWallet,
     BsFillBellFill,
     BsChevronDown,
+    HiPlus,
     PROFILE_PIC,
     COVER_BAND,
     COVER_IMAGE

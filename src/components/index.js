73ac010ffe6345c965d201ea3cfdd5997edb5e23
sri-rishi <sririshi2612@gmail.com>
2024-02-04 18:47:18 +0530

    import component and style of herosection

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Button from "./button/Button";
 import Navbar from "./navbar/Navbar";
+import HeroSection from "./heroSection/HeroSection";
 
 export {
     Button,
-    Navbar
+    Navbar,
+    HeroSection
 }

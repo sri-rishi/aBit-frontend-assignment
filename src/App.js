d71fbe60f559c07244dd199188daba110e4ce4d6
sri-rishi <sririshi2612@gmail.com>
2024-02-04 12:44:52 +0530

    feat: add Navbar component to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
 import './App.css';
+import { Navbar } from './components';
 
 function App() {
 
   useEffect(() => {
     document.title = "aBit Assignment"
-  }, [])
+  }, []);
+  
   return (
     <div className="App">
-      Hello
+      <Navbar />
     </div>
   );
 }

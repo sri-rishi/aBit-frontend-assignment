628a3d5c6a4e4adca18f7a6bdf2f05d9f27cc050
sri-rishi <sririshi2612@gmail.com>
2024-02-05 00:45:24 +0530

    feat: create footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-sub-div">
+                <div className="footer-title-box">
+                    <p>aBit</p>
+                    <p className="footer-sub-title">Changing The Way In Which Creators And Fans Interact.</p>
+                </div>
+                <div className="footer-links-box">
+                    <div className="vr-line"></div>
+                    <div className="footer-links">
+                        <p>Home</p>
+                        <p>Are you a Creator?</p>
+                        <p>Support</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;

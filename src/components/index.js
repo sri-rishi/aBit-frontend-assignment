3b15e8a31a9f28e9accccc5ccbaa6be087fdedb2
sri-rishi <sririshi2612@gmail.com>
2024-02-04 23:54:37 +0530

    import component and styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,15 @@
 import Button from "./button/Button";
 import Navbar from "./navbar/Navbar";
 import HeroSection from "./heroSection/HeroSection";
+import ReleasesSection from "./releasesSection/ReleasesSection";
+import AddVideoCard from "./addVideoCard/AddVideoCard";
+import VideoCard from "./videoCard/VideoCard";
 
 export {
     Button,
     Navbar,
-    HeroSection
+    HeroSection,
+    ReleasesSection,
+    AddVideoCard,
+    VideoCard
 }

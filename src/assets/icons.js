c80f53977fa9fbeb4132d1c7b77e5a4fbf269c6e
sri-rishi <sririshi2612@gmail.com>
2024-02-04 12:40:55 +0530

    style: add icons and image

---
+++ src/assets/icons.js
@@ -0,0 +1,9 @@
+import { IoWallet } from "react-icons/io5";
+import { BsFillBellFill } from "react-icons/bs";
+import { BsChevronDown } from "react-icons/bs";
+
+export {
+    IoWallet,
+    BsFillBellFill,
+    BsChevronDown
+}
---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import {IoWallet,BsFillBellFill,BsChevronDown} from "./icons";
+import PROFILE_PIC from "./images/profile-pic.png";
+
+export {
+    IoWallet,
+    BsFillBellFill,
+    BsChevronDown,
+    PROFILE_PIC,
+}

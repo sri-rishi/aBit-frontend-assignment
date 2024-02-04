8b4715167028a03141b00ab99aae8565d22e22fc
sri-rishi <sririshi2612@gmail.com>
2024-02-04 18:40:22 +0530

    style: import images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,13 @@
 import {IoWallet,BsFillBellFill,BsChevronDown} from "./icons";
 import PROFILE_PIC from "./images/profile-pic.png";
+import COVER_IMAGE from "./images/cover-image.png";
+import COVER_BAND from "./images/cover-band.png";
 
 export {
     IoWallet,
     BsFillBellFill,
     BsChevronDown,
     PROFILE_PIC,
+    COVER_BAND,
+    COVER_IMAGE
 }

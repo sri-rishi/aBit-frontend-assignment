5f742de1e849b9b1e347d6833db80fac98a4c256
sri-rishi <sririshi2612@gmail.com>
2024-02-04 23:52:42 +0530

    feat: create add video card component

---
+++ src/components/addVideoCard/AddVideoCard.jsx
@@ -0,0 +1,14 @@
+import {HiPlus} from "../../assets";
+
+const AddVideoCard = () => {
+    return(
+        <div className="add-video-card">
+            <HiPlus />
+            <div className="add-video-card-text">
+                <p>You haven’t uploaded any videos with aBit yet. Add now!</p>
+            </div>
+        </div>
+    )
+}
+
+export default AddVideoCard;
